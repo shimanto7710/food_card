0aac97c6d1439868624de4ce6e201eca2ad831e1
afser7710 <afser.uddin@bengalmobileqa.com>
2023-02-06 04:57:24 +0600

    working on qr code

---
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -0,0 +1,22 @@
+import 'package:get/get.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+
+class HomeController extends GetxController {
+  //TODO: Implement HomeController
+  QRViewController? qrViewController;
+  Barcode? result;
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {}
+
+}
